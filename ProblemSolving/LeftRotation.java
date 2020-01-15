a80aa7ca83ce20609f313613497f5ecfe87167d5
Md Abdullah Azad Azad <mdabaza1@in.ibm.com>
2020-01-15 10:02:29 +0530

    setup base code

---
+++ ProblemSolving/LeftRotation.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class LeftRotation {
+
+
+    static int[] rotLeft(int[] a, int d) {
+        int n = a.length;
+        int[] rotArray = new int[n];
+
+        for (int oldIndex = 0; oldIndex < n; oldIndex++) {
+            int newIndex = (oldIndex + n - d) % n;
+            rotArray[newIndex] = a[oldIndex];
+        }
+
+        return rotArray;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int res[] = rotLeft(a, d);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+        
+        
+        scanner.close();
+    }
+}
