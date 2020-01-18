614f54a57ac93323292297932188f7d48c0b982d
Md Abdullah Azad Azad <mdabaza1@in.ibm.com>
2020-01-18 10:47:17 +0530

    solved linkedlist problem for printing node & insert node at tail.

--- ProblemSolving/ArrayDS.java
+++ ProblemSolving/ArrayDS.java
@@ -1,12 +1,7 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
-public class Solution {
+public class ArrayDS {
 
     // Complete the reverseArray function below.
     static int[] reverseArray(int[] a) {
---
+++ ProblemSolving/InsertNodeAtTail.java
@@ -0,0 +1,60 @@
+import java.io.*;
+
+public class InsertNodeAtTail {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+
+        if (head == null) {
+            head = new SinglyLinkedListNode(data);
+        } else {
+        SinglyLinkedListNode node = head;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = new SinglyLinkedListNode(data);
+        }
+        return head;
+    }
+}
